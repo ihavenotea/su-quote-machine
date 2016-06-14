848a39bb8444a4e29c1ef191f5bd72409947e655
February Keeney <ihavenotea@github.com>
2016-06-14 13:01:08 -0700

    Initial import of Important Files

---
+++ quote-generator.js
@@ -0,0 +1,21 @@
+var quotes = [
+  { content: "You always say you'll be the crocodile, but you never commit!",
+    character: 'Pearl' },
+  { content: "Love at first sight doesn't exist. Love takes time and love takes work. At the very least, you have to know the other person. And you literally have no idea who or what I am.",
+    character: 'Garnet' },
+  { content: "APPARENTLY MORE THAN YOU, YOU... CLOD! ",
+    character: "Peridot" }
+];
+
+var displayQuote = function() {
+
+  var index = Math.floor((Math.random() * quotes.length));
+  var quote = quotes[index];
+  var blockquoteElement = document.getElementById('quote-body');
+  var attributionElement = document.getElementById('quote-attribution');
+
+  blockquoteElement.innerHTML = quote.content;
+  attributionElement.innerHTML = quote.character;
+}
+
+document.addEventListener("DOMContentLoaded", displayQuote);
